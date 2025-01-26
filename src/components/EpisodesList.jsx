0f8d97117a5d6358589ab993882403465b3b9d10
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-01-26 14:36:48 +0100

    add EpisodesList component; implement episode display for selected season

---
+++ src/components/EpisodesList.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+import { useEffect } from "react";
+import { useState } from "react";
+
+const EpisodesList = ({ selectedSeason, seasons, id, episodes }) => {
+  //   const [selectedEpisode, setSelectedEpisode] = useState(1);
+  //   const [loading, setLoading] = useState(false);
+
+  //   useEffect(() => {
+  //     const url = `https://api.themoviedb.org/3/tv/${id}/season/${selectedSeason}/episode/${selectedEpisode}?language=en-US`;
+  //     // const fetch
+  //   }, [id, selectedSeason, selectedEpisode]);
+
+  return (
+    <div className="bg-emerald-600 p-2 flex items-baseline rounded gap-2 w-[calc(33.33%-2rem)] hover:cursor-pointer">
+      <p className="font-oswald font-semibold">
+        Eps {episodes.episode_number}:{" "}
+      </p>
+      <p className="font-source">
+        {episodes.name.length > 25
+          ? episodes.name.substring(0, 25) + "..."
+          : episodes.name}
+      </p>
+    </div>
+  );
+};
+
+export default EpisodesList;
