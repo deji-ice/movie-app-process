226f47d85adc0cd862205e9d8bd39db7b12f3512
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-02-01 03:23:13 +0100

    fix: conditionally render SideBar and manage body overflow based on route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import MovieDetails from "./pages/MovieDetails";
 import NotFound from "./pages/NotFound";
@@ -6,14 +6,28 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import SideBar from "./components/SideBar";
 import WatchNow from "./pages/WatchNow";
+import { useEffect } from "react";
 
 const App = () => {
+  const pathname = useLocation().pathname;
+
+  const hideSideBar =
+    pathname.includes("watch-movie") || pathname.includes("watch-tv");
+
+  useEffect(() => {
+    if (hideSideBar) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [hideSideBar]);
+
   return (
     <div className="text-[#e2e2e2] bg h-full flex flex-col m-0 ">
       <Header />
       <div className="flex">
         {/* <SideBar /> */}
-        <SideBar />
+        {!hideSideBar && <SideBar />}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movie/:id" element={<MovieDetails />} />
